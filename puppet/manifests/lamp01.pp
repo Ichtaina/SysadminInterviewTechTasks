4f0ed5a818650aa7c01cb48852c00e82d1b344bc
ichtaina <marc@marcrafols.com>
2015-12-16 14:21:19 +0000

    Puppet structure added

---
+++ puppet/manifests/lamp01.pp
@@ -0,0 +1,24 @@
+lamp01 {
+	include apache
+
+    class { '::mysql::server':
+
+        # Set the root password
+        root_password => "m4rcr4f0ls",
+
+        # Create the database
+        databases => {
+            "wordpress" => {
+                ensure  => 'present',
+                charset => 'utf8'
+            }
+        }
+	}
+	user { 'blogger':
+	  comment  => 'Blogger user',
+	  home     => '/home/blogger',
+	  password => 'wordpresssecuritysucks',
+	  shell    => '/bin/bash',
+	  ensure   => present
+	}
+}
